a91c77787cdabf1f6e3152036ebc3f8f2868436f
Samir Kahvedzic <samirkafa@gmail.com>
2016-11-18 21:50:13 +0100

    Finish main and exceptions

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
+	
+	static Scanner input = new Scanner (System.in);
 
 	public static void main(String[] args) {
 
@@ -8,6 +13,19 @@ public class App {
 		 * array, ne nužno), program treba da vrati:
 		 */
 	
+	
+	System.out.println("Unesite duzinu niza ");
+	int arrayLength= exceptionArrayLength();
+	
+	int array [] = new int [arrayLength];
+	
+	System.out.println("Unesite " + arrayLength + " cijelih brojeva u niz" );
+	
+	for (int i = 0; i < array.length; i++) {
+		array [i] = exceptionInputToArray();	
+	}
+	smalestNum(array);
+	largestNumArray(array);
 	
 	
 	
@@ -86,4 +104,61 @@ public class App {
 	public static void sortArray(int[] array) {
 
 	}
+	//Metoda provjerava unos korisnika i onemogucava mu da postavi duzinu niza manju od 1
+	public static int exceptionArrayLength (){
+		
+			int unos = 0;	// pocetna vrijednost
+			boolean inputCheck = true;	//kontrolna promjenljiva
+			do {
+				try {
+					unos = input.nextInt();	//ucitavanje unosa i provjera da li je negativan broj
+					if (unos < 1) {
+						System.out.println("Pogresan unos, duzina niza mora biti minimanlno 2: ");	//ako jeste ispisi poruku
+						inputCheck = true;
+					}				
+					else inputCheck = false;	//ako je sve ok, kontrolna promjenljiva false, vrati unos									
+				}
+				catch (InputMismatchException ex) {	//u slucaju pogresnog unosa
+					System.out.println("Pogresan unos. Pokusajte ponovo: ");
+					input.nextLine();
+				}
+			
+				catch (Exception e) {
+					System.out.println("Doslo je do greske, unesite ponovo");
+					input.nextLine();
+					
+				}
+			
+			}while (inputCheck);
+			return unos;
+		
+
+	}
+	//Metoda sprecava pogresan onos korisnika
+	public static int exceptionInputToArray(){
+		int unos = 0;	// pocetna vrijednost
+		boolean inputCheck = true;	//kontrolna promjenljiva
+		do {
+			try {unos = input.nextInt();	//ucitavanje unosa i provjera da li je unesen znak
+				inputCheck=false;							
+			}
+			catch (InputMismatchException ex) {	//u slucaju pogresnog unosa
+				System.out.println("Pogresan unos. Pokusajte ponovo, unesite cijele brojeve: ");
+				input.nextLine();
+			}
+		
+			catch (Exception e) {
+				System.out.println("Doslo je do greske, unesite ponovo");
+				input.nextLine();
+				
+			}
+		
+		}while (inputCheck);
+		return unos;
+	
+
+	}
 }
+		
+	
+
