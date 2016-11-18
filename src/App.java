0417c722fa568b4d40ded691ff4a1b8a9ebdf67c
Samir Kahvedzic <samirkafa@gmail.com>
2016-11-18 09:35:43 +0100

    Initializatio setup

---
+++ src/App.java
@@ -0,0 +1,62 @@
+public class App {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Napisati program koji pita korisnika da unese niz brojeva (double ili
+		 * cijelih, vi odluèite). Nakon što je korisnik unio niz brojeva (niz !=
+		 * array, ne nužno), program treba da vrati:
+		 */
+	}
+
+	/*
+	 * Metoda treba da vrati: Najmanji broj u nizu
+	 */
+	public static void smalestNum(int[] array) {
+
+	}
+
+	/*
+	 * Metoda treba da vrati: Najveæi broj u nizu
+	 */
+	public static void largestNumArray(int[] array) {
+
+	}
+
+	/*
+	 * Metoda treba da vrati: Prosjek brojeva u nizu
+	 */
+	public static void sumArray(int[] array) {
+
+	}
+
+	/*
+	 * Metoda treba da vrati: Prosjek brojeva u nizu
+	 */
+
+	public static void averageNumArray(int[] array) {
+
+	}
+
+	/*
+	 * Metoda treba da vrati: Sve unikatne brojeve u nizu
+	 */
+	public static void uniqueNumArray(int[] array) {
+
+	}
+
+	/*
+	 * Metoda treba da vrati: Prosjek brojeva u nizu
+	 */
+	public static void smalestNumArray(int[] array) {
+
+	}
+
+	/*
+	 * Metoda treba da vrati: Unijeti niz,sortiran od najmanjeg ka najveæem
+	 * broju
+	 */
+	public static void sortArray(int[] array) {
+
+	}
+}
